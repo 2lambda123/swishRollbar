73980e628a87510e97f2bb1e342eea73beddf5c5
Patrik <patrikwebb@hotmail.com>
2017-11-28 22:20:11 +0100

    Updated docker rebuld script.

--- docker-rebuild.sh
+++ docker-rebuild.sh
@@ -2,5 +2,5 @@
 
 docker rm -f swishRollbar
 docker rmi -f swishRollbar
-docker build -t rp-swishserver .
-docker run -p 3000:3000 -dit --name swishRollbar rp-swishserver
+docker build -t rp-swishrollbar .
+docker run -p 3000:3000 -dit --name swishRollbar rp-swishrollbar
